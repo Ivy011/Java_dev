a23369ff45d2f18bee4c101ae1170d19c73339cb
Ivy011 <115562322+Ivy011@users.noreply.github.com>
2023-09-09 10:38:21 -0400

    week1

--- a1-hello-world-main/0-hello-world/hello-world.js
+++ a1-hello-world-main/0-hello-world/hello-world.js
@@ -1,7 +1,7 @@
 // Your code here
 const helloWorld = () => {
-  let response = '';
-
+  let response = 'Hello, World!';
+  console.log('Hello, World!')
   return response;
 }
 // End your code
--- a1-hello-world-main/1-alice-bob/alice-bob.js
+++ a1-hello-world-main/1-alice-bob/alice-bob.js
@@ -1,7 +1,15 @@
 // Your code here
 const respond = (name, age) => {
   let response = '';
-  
+  if (age<20){
+    response = `Hi ${name}! You are younger than me.`
+  } else {
+    response = `Hi ${name}! You are older than me.`
+  } 
+
+  // if (age>=20){
+  //   response = `Hi ${name}! You are older than me.`
+  // } 
   return response;
 }
 // End your code
